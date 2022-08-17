4129a3b53741171978f760251bb2ddb8face1bc5
GentleCold <1952173800@qq.com>
2022-08-18 01:04:06 +0800

    feat: finish chain

--- Algorithm/LinearList/chain.h
+++ Algorithm/LinearList/chain.h
@@ -1,6 +1,6 @@
 //
 // Created by GentleCold on 2022/8/17.
-// 由链表描述的线性表模板类
+// 由单向链表描述的线性表模板类
 //
 
 #ifndef MYTINYALGORITHM_CHAIN_H
@@ -29,34 +29,163 @@ struct ChainNode {
 template <class T>
 class Chain : public LinearList<T> {
 public:
-    Chain(int initialCapacity = 10);
+    Chain();
     Chain(const Chain<T>&);
-    ~Chain() { delete [] _array; }
+    ~Chain();
 
     // ADT
     bool empty() const { return _size == 0; }
     int size() const { return _size; }
     T& get(int index) const;
-    int indexOf(const T& element) const;
+    int index_of(const T& value) const;
     void erase(int index);
-    void insert(int index, const T& element);
-    virtual void push_back(const T& element);
-    virtual void clear();
+    void insert(int index, const T& value);
+    void push_back(const T& value);
+    void clear();
 
-    // more
-    int capacity() const { return capacity; }
 
     // iterator
     class iterator;
-    iterator begin() { return iterator(_array); }
-    iterator end() { return iterator(_array + _capacity); }
+    iterator begin() { return iterator(_head -> next); }
+    iterator end() { return iterator(nullptr); }
 
 protected:
-    T* _array = nullptr;
+    ChainNode<T> *_head = nullptr;
     int _size = 0;
-    int _capacity = 0;
 };
 
+template <class T>
+Chain<T>::Chain() {
+    _head = new ChainNode<T>;
+}
+
+template <class T>
+Chain<T>::Chain(const Chain<T>& v) {
+    _size = v._size;
+    _head = new ChainNode<T>;
+    ChainNode<T> *leftNode = _head;
+    ChainNode<T> *rightNode = v._head -> next;
+    for (int i = 0; i < _size; i++) {
+        leftNode -> next = new ChainNode<T>(rightNode -> value);
+        leftNode = leftNode -> next;
+        rightNode = rightNode -> next;
+    }
+}
+
+template <class T>
+Chain<T>::~Chain() {
+    while (_head != nullptr) {
+        ChainNode<T> *p = _head;
+        _head = _head -> next;
+        delete p;
+    }
+}
+
+template <class T>
+T& Chain<T>::get(int index) const {
+    assert(index >= 0 && index < _size);
+
+    ChainNode<T> *currentNode = _head -> next;
+    while (index) {
+        index--;
+        currentNode = currentNode -> next;
+    }
+
+    return currentNode -> value;
+}
+
+template <class T>
+int Chain<T>::index_of(const T& v) const {
+    int index = 0;
+    ChainNode<T> *currentNode = _head -> next;
+
+    while (currentNode) {
+        if (currentNode -> value == v) {
+            return index;
+        }
+        currentNode = currentNode -> next;
+        index++;
+    }
+
+    return -1;
+}
+
+template <class T>
+void Chain<T>::erase(int index) {
+    assert(index >= 0 && index < _size);
+
+    ChainNode<T> *p = _head;
+    while (index) {
+        index--;
+        p = p -> next;
+    }
+
+    ChainNode<T> *deleteNode = p -> next;
+    p -> next = p -> next -> next;
+    delete deleteNode;
+    _size--;
+}
+
+template<class T>
+void Chain<T>::insert(int index, const T& v) {
+    assert(index >= 0 && index <= _size);
+
+    ChainNode<T> *p = _head;
+    while (index) {
+        index--;
+        p = p -> next;
+    }
+    p -> next = new ChainNode<T>(v, (p -> next) ? p -> next -> next : nullptr);
+    _size++;
+}
+
+template<class T>
+void Chain<T>::push_back(const T &v) {
+    insert(_size, v);
+}
+
+template<class T>
+void Chain<T>::clear() {
+    ChainNode<T> *p = _head -> next;
+    while (p) {
+        ChainNode<T> *t = p;
+        p = p -> next;
+        delete t;
+    }
+    _size = 0;
+    _head -> next = nullptr;
+}
+
+template <class T>
+class Chain<T>::iterator {
+public:
+    // constructor
+    explicit iterator(ChainNode<T> *node = nullptr) { this -> node = node; }
+
+    // de-referencing operators
+    T& operator*() const { return node -> value; }
+    T* operator->() const { return &node -> value; }
+
+    // increment
+    iterator& operator++() { node = node -> next; return *this; }
+    iterator operator++(int) {
+        iterator old = *this;
+        node = node -> next;
+        return old;
+    }
+
+    // equality testing
+    bool operator!=(const iterator right) const {
+        return node != right.node;
+    }
+
+    bool operator==(const iterator right) const{
+        return node == right.node;
+    }
+
+protected:
+    ChainNode<T>* node;
+};
 
 
 }
--- Test/test_linear_list.h
+++ Test/test_linear_list.h
@@ -22,6 +22,7 @@ static void test_array_list() {
     EXPECT_EQ_INT(11, v1.get(0));
     EXPECT_EQ_INT(3, v1.get(2));
     EXPECT_EQ_INT(1, v1.index_of(2));
+    EXPECT_EQ_INT(-1, v1.index_of(100));
     EXPECT_EQ_INT(3, v1.size());
     EXPECT_EQ_INT(3, v1.capacity());
     v1.erase(1);
@@ -36,7 +37,27 @@ static void test_array_list() {
 }
 
 static void test_chain() {
-    // myalm::Chain<int> v;
+    myalm::Chain<int> v;
+
+    EXPECT_EQ_TRUE(v.empty());
+    v.push_back(11);
+    v.push_back(2);
+    v.push_back(3);
+    myalm::Chain<int> v1(v);
+    EXPECT_EQ_INT(16, std::accumulate(v1.begin(), v1.end(), 0));
+    EXPECT_EQ_INT(11, v1.get(0));
+    EXPECT_EQ_INT(3, v1.get(2));
+    EXPECT_EQ_INT(1, v1.index_of(2));
+    EXPECT_EQ_INT(-1, v1.index_of(100));
+    EXPECT_EQ_INT(3, v1.size());
+    v1.erase(1);
+    EXPECT_EQ_INT(2, v1.size());
+    v1.insert(0, 1);
+    EXPECT_EQ_INT(1, v1.get(0));
+    v1.clear();
+    EXPECT_EQ_INT(0, v1.size());
+    v1.push_back(1);
+    v1.erase(0);
 }
 
 void test_linear_list() {
